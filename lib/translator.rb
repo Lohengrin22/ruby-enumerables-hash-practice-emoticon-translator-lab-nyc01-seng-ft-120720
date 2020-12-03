ba3f329f1da079cf4bb8ac12014043e8b1766f5f
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-03 20:20:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,7 @@ end
 
 def get_japanese_emoticon(file, emoticon)
   new_file = load_library(file)
+  japanese_icon = ""
   
 end
 
@@ -27,7 +28,7 @@ def get_english_meaning(file, emoticon)
     
     if japanese_icon == emoticon
      english_meaning = meaning 
-    else 
+    elsif emoticon != values[:japanese] && values[:english] 
     sorry_message 
    end 
   end 
